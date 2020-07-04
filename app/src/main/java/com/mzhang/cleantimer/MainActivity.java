0f3b9c18d09e3fc5a90370dc3a604bb17397ee4c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 13:14:06 -0400

    Made the app launch into immersive mode!

--- app/src/main/java/com/mzhang/cleantimer/MainActivity.java
+++ app/src/main/java/com/mzhang/cleantimer/MainActivity.java
@@ -83,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,8 +93,15 @@ public class MainActivity extends AppCompatActivity {
             setTheme(R.style.LightTheme);
         }
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
