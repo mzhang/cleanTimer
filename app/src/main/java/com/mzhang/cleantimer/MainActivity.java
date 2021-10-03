278a18cc763eaadd88ae0284a6f16f9e93a2e4cf
Matt Zhang <zugzhang@gmail.com>
2021-10-03 13:57:16 -0400

    No longer forces immersive mode

--- app/src/main/java/com/mzhang/cleantimer/MainActivity.java
+++ app/src/main/java/com/mzhang/cleantimer/MainActivity.java
@@ -253,13 +253,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         timer = findViewById(R.id.timer);
--- app/src/main/java/com/mzhang/cleantimer/stats.java
+++ app/src/main/java/com/mzhang/cleantimer/stats.java
@@ -93,13 +93,6 @@ public class stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
         SharedPreferences pref = getSharedPreferences("com.mzhang.cleantimer.isDark", Context.MODE_PRIVATE);
         isDark = pref.getBoolean("isDark", true);
         if (isDark) {
